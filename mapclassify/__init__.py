3422ae4e2f2ea417a79b7ccbec60c7a644048d77
Serge Rey <sjsrey@gmail.com>
2020-06-07 10:46:22 -0700

    ENH: classify wrapper function

--- mapclassify/__init__.py
+++ mapclassify/__init__.py
@@ -25,3 +25,41 @@ from .classifiers import (
 
 from .pooling import Pooled
 from .greedy import greedy
+
+dispatch = {}
+for classifier in CLASSIFIERS:
+    dispatch[classifier.lower()] = eval(classifier)
+
+
+def classify(y, method="quantiles", **kwargs):
+    """
+    Helper function to dispatch specified classifier
+
+
+    Parameters
+    ----------
+
+    y: array (n,1)
+        Values to be classified
+
+    method: string
+         Name of classification method to used
+
+    kwargs: dict
+         Optional parameters for classifier
+
+
+    Returns
+    -------
+
+    classified: MapClassifier 
+
+
+
+    """
+
+    ml = method.lower()
+    if ml in dispatch:
+        return dispatch[ml](y, **kwargs)
+    else:
+        print(f'{method} not a known classifier.')
